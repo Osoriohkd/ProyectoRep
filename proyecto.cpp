93561d234094b6d524a5a39c39efecb443c16053
Osoriohkd <jhordy.osorio@correounivalle.edu.co>
2025-05-12 08:09:31 -0500

    proyecto3

--- proyecto.cpp
+++ proyecto.cpp
@@ -4,4 +4,5 @@ using namespace std;
 int main {
     cout << "Soy jhordy " << endl; 
     cout << "Codigo: 2419807" << endl; 
+    cout << "Materia: Metodologias de Desarrollo de Software" << endl;
 }
